c531dfa7eb675ea619759b71a3eb96cc70803784
ªL®m½n <wilson.lin@nadisystem.com>
2021-08-18 19:46:26 +0800

    update

--- Tutorial - base/SQL_app/database.py
+++ Tutorial - base/SQL_app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "jdbc:sqlite:C:\Users\NADI\Desktop\SQLite\test.ini"
+SQLALCHEMY_DATABASE_URL = "sqlite://D:/project/fastapi_test/Tutorial - base/SQL_app/test.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
