dda84f34e6e7c5202d6cd4958ab2caa4ef37a6f6
Wilson.Lin <wilson.lin@nadisystem.com>
2021-11-19 18:02:17 +0800

    update

--- Tutorial_base/SQL_app/database.py
+++ Tutorial_base/SQL_app/database.py
@@ -2,12 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = r"sqlite:///C:\Users\NADI\Test.db"
+# SQLALCHEMY_DATABASE_URL = r"sqlite:///C:\Users\NADI\Test.db"
 # SQLALCHEMY_DATABASE_URL = "sqlite:///Test.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:123456@localhost:3306/test"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- Tutorial_base/SQL_app/models.py
+++ Tutorial_base/SQL_app/models.py
@@ -9,8 +9,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    email = Column(String(64), unique=True, index=True)
+    hashed_password = Column(String(64))
     is_active = Column(Boolean, default=True)
 
     items = relationship("Item", back_populates="owner")
@@ -20,8 +20,8 @@ class Item(Base):
     __tablename__ = "items"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
+    title = Column(String(64), index=True)
+    description = Column(String(64), index=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="items")
